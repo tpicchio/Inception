9077fb65369d6cd9d31701680e7ca653af514471
tpicchio <picchiotommaso99@gmail.com>
2024-12-12 15:09:07 +0100

    Inception with bonuses

--- srcs/requirements/mariadb/conf/setup_db.sh
+++ srcs/requirements/mariadb/conf/setup_db.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-# Inizializzazione del database (se non è già stato fatto)
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     chown -R mysql:mysql /var/lib/mysql
-    # Inizializzazione del database per MariaDB
     mysqld --user=mysql --initialize --basedir=/usr --datadir=/var/lib/mysql
 fi
 
-# Creazione del database e dell'utente (se non esistono già)
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
     cat << EOF > /tmp/setup_db.sql
 USE mysql;
@@ -26,7 +23,6 @@ CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
 GRANT ALL PRIVILEGES ON inception_db.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-    # Esegui il file di script di inizializzazione
     /usr/bin/mysqld --user=mysql --bootstrap < /tmp/setup_db.sql
     rm -f /tmp/setup_db.sql
 fi
